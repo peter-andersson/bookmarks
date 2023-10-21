a26f83ca9b283d8a6132e85f20d2be5d437a6eeb
Peter Andersson <peter@lpda.se>
2023-10-21 09:52:46 +0200

    Removed databaseproject

--- src/Database/public/table/tag.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists tag
-(
-    id   integer generated always as identity
-        constraint tag_pk
-            primary key,
-    name varchar(50) not null
-);
-
--- src/Database/public/table/version_info.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists version_info
-(
-    version     integer       not null
-        constraint version_info_pk
-            primary key,
-    applied     timestamp     not null,
-    description varchar(1000) not null
-);
-
